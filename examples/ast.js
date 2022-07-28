0333e92656c87a814d4197c90313b68a997d8ca4
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-07-27 22:12:36 -0600

    Update interface

--- examples/ast.js
+++ examples/ast.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
 const mo = require('..');
 
 const actorMo = `
-import Nat "mo:base/Nat";
+// import Nat "mo:base/Nat";
 
 actor Main {
     public func test() : async Nat {
@@ -12,47 +12,21 @@ actor Main {
 }
 `;
 
-// const actorMo = `
-// actor {
-//     public func test() : async Nat {
-//         123
-//     }
-// }
-// `;
-
-// const baseInfo = {
-//     name: 'base',
-//     repo: 'https://github.com/dfinity/motoko-base.git',
-//     dir: 'src',
-//     version: 'master',
-//     homepage: 'https://sdk.dfinity.org/docs/base-libraries/stdlib-intro.html',
-// };
-
 (async () => {
-    // await mo.loadPackage(baseInfo);
-    await mo.loadPackages({
-        base: 'dfinity/motoko-base/master/src',
-    });
+    // await mo.loadPackages({
+    //     base: 'dfinity/motoko-base/master/src',
+    // });
 
     mo.addFile('Main.mo', actorMo);
 
     // let x = Motoko.compileWasm('ic', 'Main.mo');
     // console.log(x);
 
-    const visit = (node) => {
-        console.log(node);
-
-        for (let arg of node.args) {
-            console.log(arg);
-        }
-    };
-
     const ast = mo.parse(actorMo);
-
     // console.log(ast);
 
-    console.log(mo.getFiles('.'));
-    console.log(mo.getFiles('base'));
+    console.log(mo.listFiles('.'));
+    // console.log(mo.listFiles('base'));
 
     const candid = mo.candid('Main.mo');
     console.log(candid);
@@ -60,5 +34,7 @@ actor Main {
     const candidAST = mo.parseCandid(candid);
     console.log(candidAST);
 
-    console.log(JSON.stringify(ast, null, 1));
+    // mo.wasm('wasi', 'Main.mo');
+
+    // console.log(JSON.stringify(ast, null, 1));
 })().catch((err) => console.error(err.stack || err));
