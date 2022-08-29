44f1b5b6bc163df5886b7d6f4a28bbb474761066
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-08-29 10:08:24 -0600

    Fix access modifier keywords

--- contrib/monaco.js
+++ contrib/monaco.js
@@ -1,44 +1,44 @@
-'use strict'
+'use strict';
 
 /// Monaco editor configuration
 exports.configure = (monaco) => {
-    monaco.languages.register({ id: "motoko" });
-    monaco.languages.setLanguageConfiguration("motoko", {
+    monaco.languages.register({ id: 'motoko' });
+    monaco.languages.setLanguageConfiguration('motoko', {
         comments: {
-            lineComment: "//",
-            blockComment: ["/*", "*/"],
+            lineComment: '//',
+            blockComment: ['/*', '*/'],
         },
         brackets: [
-            ["{", "}"],
-            ["[", "]"],
-            ["(", ")"],
+            ['{', '}'],
+            ['[', ']'],
+            ['(', ')'],
         ],
         autoClosingPairs: [
-            { open: "{", close: "}" },
-            { open: "[", close: "]" },
-            { open: "(", close: ")" },
+            { open: '{', close: '}' },
+            { open: '[', close: ']' },
+            { open: '(', close: ')' },
             { open: '"', close: '"' },
-            { open: "<", close: ">" },
+            { open: '<', close: '>' },
         ],
     });
-    monaco.languages.setMonarchTokensProvider("motoko", {
-        defaultToken: "",
-        tokenPostfix: ".mo",
+    monaco.languages.setMonarchTokensProvider('motoko', {
+        defaultToken: '',
+        tokenPostfix: '.mo',
         // prettier-ignore
         keywords: [
             'actor', 'and', 'async', 'assert', 'await', 'break', 'case', 'catch', 'class',
-            'continue', 'debug', 'debug_show', 'do', 'else', 'false', 'for', 'from_candid', 'func', 'if', 'ignore', 'in', 'import',
-            'label', 'let', 'loop', 'module', 'not', 'null', 'object', 'or', 'return',
-            'switch', 'throw', 'to_candid', 'true', 'try', 'type', 'var', 'while', 'with',
+            'continue', 'debug', 'debug_show', 'do', 'else', 'false', 'flexible', 'for', 'from_candid', 'func', 'if', 'ignore', 'in',
+            'import', 'label', 'let', 'loop', 'module', 'not', 'null', 'object', 'or', 'private', 'public', 'query', 'return', 'shared',
+            'stable', 'switch', 'system', 'throw', 'to_candid', 'true', 'try', 'type', 'var', 'while', 'with',
         ],
         accessmodifiers: [
-            "public",
-            "private",
-            "flexible",
-            "query",
-            "shared",
-            "stable",
-            "system",
+            'public',
+            'private',
+            'flexible',
+            'query',
+            'shared',
+            'stable',
+            'system',
         ],
         // prettier-ignore
         typeKeywords: [
@@ -62,55 +62,68 @@ exports.configure = (monaco) => {
                     /[a-zA-Z_$][\w$]*/,
                     {
                         cases: {
-                            "@typeKeywords": "keyword.type",
-                            "@keywords": "keyword",
-                            "@default": "identifier",
+                            '@typeKeywords': 'keyword.type',
+                            '@keywords': 'keyword',
+                            '@default': 'identifier',
                         },
                     },
                 ],
                 // whitespace
-                { include: "@whitespace" },
+                { include: '@whitespace' },
 
                 // delimiters and operators
-                [/[{}()[\]]/, "@brackets"],
-                [/[<>](?!@symbols)/, "@brackets"],
-                [/@symbols/, { cases: { "@operators": "operator", "@default": "" } }],
+                [/[{}()[\]]/, '@brackets'],
+                [/[<>](?!@symbols)/, '@brackets'],
+                [
+                    /@symbols/,
+                    { cases: { '@operators': 'operator', '@default': '' } },
+                ],
                 // numbers
-                [/\d*\.\d+([eE][-+]?\d+)?/, "number.float"],
-                [/0[xX][0-9a-fA-F_]+/, "number.hex"],
-                [/[0-9_]+/, "number"],
+                [/\d*\.\d+([eE][-+]?\d+)?/, 'number.float'],
+                [/0[xX][0-9a-fA-F_]+/, 'number.hex'],
+                [/[0-9_]+/, 'number'],
 
                 // delimiter: after number because of .\d floats
-                [/[;,.]/, "delimiter"],
+                [/[;,.]/, 'delimiter'],
 
                 // strings
-                [/"([^"\\]|\\.)*$/, "string.invalid"], // non-teminated string
-                [/"/, { token: "string.quote", bracket: "@open", next: "@string" }],
+                [/"([^"\\]|\\.)*$/, 'string.invalid'], // non-teminated string
+                [
+                    /"/,
+                    {
+                        token: 'string.quote',
+                        bracket: '@open',
+                        next: '@string',
+                    },
+                ],
 
                 // characters
-                [/'[^\\']'/, "string"],
-                [/(')(@escapes)(')/, ["string", "string.escape", "string"]],
-                [/'/, "string.invalid"],
+                [/'[^\\']'/, 'string'],
+                [/(')(@escapes)(')/, ['string', 'string.escape', 'string']],
+                [/'/, 'string.invalid'],
             ],
 
             comment: [
-                [/[^/*]+/, "comment"],
-                [/\/\*/, "comment", "@push"], // nested comment
-                ["\\*/", "comment", "@pop"],
-                [/[/*]/, "comment"],
+                [/[^/*]+/, 'comment'],
+                [/\/\*/, 'comment', '@push'], // nested comment
+                ['\\*/', 'comment', '@pop'],
+                [/[/*]/, 'comment'],
             ],
 
             string: [
-                [/[^\\"]+/, "string"],
-                [/@escapes/, "string.escape"],
-                [/\\./, "string.escape.invalid"],
-                [/"/, { token: "string.quote", bracket: "@close", next: "@pop" }],
+                [/[^\\"]+/, 'string'],
+                [/@escapes/, 'string.escape'],
+                [/\\./, 'string.escape.invalid'],
+                [
+                    /"/,
+                    { token: 'string.quote', bracket: '@close', next: '@pop' },
+                ],
             ],
 
             whitespace: [
-                [/[ \t\r\n]+/, "white"],
-                [/\/\*/, "comment", "@comment"],
-                [/\/\/.*$/, "comment"],
+                [/[ \t\r\n]+/, 'white'],
+                [/\/\*/, 'comment', '@comment'],
+                [/\/\/.*$/, 'comment'],
             ],
         },
     });
