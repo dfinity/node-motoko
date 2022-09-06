ca39276069a32a9e8006f59ac4f088862643efcb
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-09-06 12:37:10 -0600

    Reformat package usage example

---
+++ examples/package.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const mo = require('..');
+
+mo.fetchPackage('dfinity/motoko-base/master/src')
+    .then(async ({ files, ...meta }) => {
+        console.log('Metadata:', meta);
+        console.log('Files:', Object.keys(files))
+    });
