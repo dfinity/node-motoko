6f1f9fe0b69c0826d296d22d21b3fb01ea535f17
rvanasa <ryan.vandersmith@dfinity.org>
2025-04-17 17:59:22 -0600

    Add test

--- tests/package.test.ts
+++ tests/package.test.ts
@@ -22,3 +22,19 @@ describe('fetchPackage', () => {
         file.run();
     });
 });
+
+describe('clearPackages', () => {
+    test('remove loaded packages', () => {
+        const file = mo.file('Test.mo');
+        file.write('import Debug "mo:custom/Debug"; Debug.print(debug_show 123)');
+
+        mo.loadPackage({
+            ...require('../packages/latest/base.json'),
+            name: 'custom',
+        });
+        file.run();
+
+        mo.compiler.clearPackage([]);
+        expect(() => file.run()).toThrow('todo error message');
+    });
+});
