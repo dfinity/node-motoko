1334d282a1d691456fe5a3c3a60dea42fd3ce6f3
Kamil Listopad <listopadkamil@gmail.com>
2025-05-29 21:51:48 +0200

    npm run generate local (#132)

--- utils/generate.js
+++ utils/generate.js
@@ -2,56 +2,99 @@
 
 const fs = require('fs/promises');
 const { join, resolve, basename } = require('path');
+const { execSync } = require('child_process');
 const axios = require('axios');
 const { fetchPackage } = require('../lib/package');
 
 const version = process.argv[2];
-if (!/\d+\.\d+\.\d+/.test(version)) {
+const isLocalBuild = version === 'local';
+
+if (!isLocalBuild && !/\d+\.\d+\.\d+/.test(version)) {
     console.error(
-        `Please pass a valid Motoko version to the 'generate' script. Received ${version}`,
+        `Please pass a valid Motoko version or 'local' to the 'generate' script. Received ${version}`,
     );
     process.exit(1);
 }
 
 const motokoRepoPath =
-    process.env.MOTOKO_REPO || resolve(__dirname, '../../motoko');
+    process.env.MOTOKO_REPO || resolve(__dirname, '../../motoko/');
 
 (async () => {
-    console.log('Downloading `moc.js` files...');
-    await fs.writeFile(
-        resolve(__dirname, `../versions/latest/moc.min.js`),
-        (
-            await axios.get(
-                `https://github.com/dfinity/motoko/releases/download/${version}/moc-${version}.js`,
-            )
-        ).data,
-    );
-    await fs.writeFile(
-        resolve(__dirname, `../versions/latest/moc_interpreter.min.js`),
-        (
-            await axios.get(
-                `https://github.com/dfinity/motoko/releases/download/${version}/moc-interpreter-${version}.js`,
-            )
-        ).data,
-    );
-
-    // TODO: download latest version of `didc.js`
-
-    console.log('Downloading base library...');
-    const basePackage = await fetchPackage(
-        'base',
-        `dfinity/motoko-base/moc-${version}/src`,
-    );
-    if (
-        basePackage.version !== `moc-${version}` ||
-        !Object.entries(basePackage.files).length
-    ) {
-        throw new Error('Unexpected package format');
+    async function copyFile(buildDir, destDir, sourceFileName, destFileName) {
+        const sourcePath = join(buildDir, sourceFileName);
+        const destPath = join(destDir, destFileName);
+        await fs.unlink(destPath);
+        await fs.copyFile(sourcePath, destPath);
     }
-    await fs.writeFile(
-        __dirname + '/../packages/latest/base.json',
-        JSON.stringify(basePackage),
-    );
+
+    if (isLocalBuild) {
+        console.log('Building Motoko from local repository...');
+        try {
+            execSync('nix develop -c make -C src moc.js moc_interpreter.js', {
+                cwd: motokoRepoPath,
+                stdio: 'inherit',
+            });
+
+            console.log('Copying `moc.js` and `moc_interpreter.js` files...');
+            const buildDir = join(motokoRepoPath, 'src/_build/default/js/');
+            const destDir = resolve(__dirname, '../versions/latest/');
+
+            await copyFile(buildDir, destDir, 'moc_js.bc.js', 'moc.min.js');
+            await copyFile(
+                buildDir,
+                destDir,
+                'moc_interpreter.bc.js',
+                'moc_interpreter.min.js',
+            );
+
+            console.log('Local `moc.js` and `moc_interpreter.js` updated.');
+            console.log('Skipping base library for local build.'); // Future work: Use local base lib repo
+        } catch (err) {
+            console.error('Error during local build or file copying:');
+            console.error(err);
+            process.exit(1);
+        }
+    } else {
+        // This block executes for versioned (non-local) builds
+        console.log(
+            `Preparing to download resources for Motoko version ${version}...`,
+        );
+        console.log('Downloading `moc.js` files...');
+        await fs.writeFile(
+            resolve(__dirname, `../versions/latest/moc.min.js`),
+            (
+                await axios.get(
+                    `https://github.com/dfinity/motoko/releases/download/${version}/moc-${version}.js`,
+                )
+            ).data,
+        );
+        await fs.writeFile(
+            resolve(__dirname, `../versions/latest/moc_interpreter.min.js`),
+            (
+                await axios.get(
+                    `https://github.com/dfinity/motoko/releases/download/${version}/moc-interpreter-${version}.js`,
+                )
+            ).data,
+        );
+
+        // TODO: download latest version of `didc.js`
+
+        console.log('Downloading base library...');
+        const basePackage = await fetchPackage(
+            'base',
+            `dfinity/motoko-base/moc-${version}/src`,
+        );
+        if (
+            basePackage.version !== `moc-${version}` ||
+            !Object.entries(basePackage.files).length
+        ) {
+            throw new Error('Unexpected package format');
+        }
+        await fs.writeFile(
+            __dirname + '/../packages/latest/base.json',
+            JSON.stringify(basePackage),
+        );
+    }
 
     console.log('Updating error code explanations...');
     const errorCodes = {};
